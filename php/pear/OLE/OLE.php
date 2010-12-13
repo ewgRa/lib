e57beadff80e719fc11539f447d4c9e65cbebcb0
Evgeniy Sokolov <ewgraf@gmail.com>
2010-12-13 20:05:36 +0000

    php 5.3 strict

--- php/pear/OLE/OLE.php
+++ php/pear/OLE/OLE.php
@@ -304,7 +304,7 @@ class OLE extends PEAR
     * @param string $ascii The ASCII string to transform
     * @return string The string in Unicode
     */
-    function Asc2Ucs($ascii)
+    static function Asc2Ucs($ascii)
     {
         $rawname = '';
         for ($i = 0; $i < strlen($ascii); $i++) {
@@ -322,7 +322,7 @@ class OLE extends PEAR
     * @param integer $date A timestamp 
     * @return string The string for the OLE container
     */
-    function LocalDate2OLE($date = null)
+    static function LocalDate2OLE($date = null)
     {
         if (!isset($date)) {
             return "\x00\x00\x00\x00\x00\x00\x00\x00";
